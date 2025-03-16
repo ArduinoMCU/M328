2a5daa203ab4510efca40f84c17696f3617d1350
ArduinoMCU <programmerdebjyoti@gmail.com>
2025-03-16 19:18:25 +0530

    m328

--- src/main.c
+++ src/main.c
@@ -7,9 +7,9 @@
 int main(void) {
     PORTB = 0xFF;   // Set as output
     while (True) {
-        PORTB = 0x00;
+        PORTB = 0x00;   // Set as low
         _delay_ms(1000);
-        PORTB = 0xFF;
+        PORTB = 0xFF;   // Set as high
         _delay_ms(1000);
     }
 
