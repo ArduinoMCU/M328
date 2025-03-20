1d0bc0c5af228fd6c960057d188270377cc1fdc2
ArduinoMCU <programmerdebjyoti@gmail.com>
2025-03-20 21:51:56 +0530

    m328

--- src/main.c
+++ src/main.c
@@ -1,13 +1,9 @@
 #define F_CPU 16000000UL
 
-#include <avr/io.h> 
+#include <avr/io.h>
 #include <util/delay.h>
 
 int main() {
-    DDRB |= (1 << DDB5); // Set pin 13 as output
-    while (1) {
-        PORTB ^= (1 << PORTB5); // Toggle pin 13
-        _delay_ms(1000); // Delay 1 second
-    }
+
     return 0;
 }
