19446d4f278d3b77dfb5f72218d790b5bfca8cdf
ArduinoMCU <programmerdebjyoti@gmail.com>
2025-03-20 21:53:29 +0530

    m328

--- src/main.c
+++ src/main.c
@@ -4,6 +4,13 @@
 #include <util/delay.h>
 
 int main() {
+    DDRB = 0x20;
+    while (1) {
+        PORTB = 0x20;
+        _delay_ms(500);
+        PORTB = 0x00;
+        _delay_ms(500);
+    }
 
     return 0;
 }
