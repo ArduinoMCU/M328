c07570e28e84a6f4f576e3d215e90308eb2ef115
ArduinoMCU <programmerdebjyoti@gmail.com>
2025-04-09 20:14:56 +0530

    M328

--- src/main.c
+++ src/main.c
@@ -1,3 +1,19 @@
-int main() {
+#define F_CPU 16000000UL 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+    // Set PORTB0 as output
+    DDRB |= (1 << DDB0);
     
+    while (1) {
+        // Toggle PORTB0
+        PORTB ^= (1 << PORTB0);
+        
+        // Wait for 500 milliseconds
+        _delay_ms(500);
+    }
+    
+    return 0;
 }
